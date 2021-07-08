587a6cd10758d8a8481749f9e056261440869f9a
Jamie <j@mieis.sexy>
2021-07-08 06:48:51 -0400

    modified: main.py

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
 	print(
-		f'READY'
+		f'READY\n'
 	)
 
 @client.event
 async def on_member_join(member):
-	print(f'JOIN: 	USER: {member}\n	USER ID:<@!{member.id}>\n	GUILD:{member.guild.name}\n	GUILD ID:{member.guild.id}')
+	print(f'JOIN: 	USER: {member}\n	USER ID:<@!{member.id}>\n	GUILD:{member.guild.name}\n	GUILD ID:{member.guild.id}\n')
 	await member.create_dm()
 	await member.dm_channel.send(
 		f'BARK BARK BARK BARK BARK'
@@ -29,6 +29,6 @@ async def on_member_join(member):
 
 @client.event
 async def on_member_remove(member):
-	print(f'LEAVE:	USER: {member}\n	USER ID:<@!{member.id}>\n	GUILD:{member.guild.name}\n	GUILD ID:{member.guild.id}')
+	print(f'LEAVE:	USER: {member}\n	USER ID:<@!{member.id}>\n	GUILD:{member.guild.name}\n	GUILD ID:{member.guild.id}\n')
 
 client.run(TOKEN)
