7f6af9930482544e50234925ecea1a8943266018
Jamie <j@mieis.sexy>
2021-07-08 03:04:43 -0400

    modified:   main.py

--- main.py
+++ main.py
@@ -7,17 +7,28 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-client = discord.Client()
+
+intents = discord.Intents.default()
+intents.members = True
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-	for guild in client.guilds:
-		if guild.name == GUILD:
-			break
-
 	print(
-		f'{client.user} has connected to the following guild:\n'
-		f'{guild.name}(id: {guild.id})'
+		f'{client.user.name} has connected to to Discord!'
 	)
+
+@client.event
+async def on_member_join(member):
+	print(f'{member.name} has joined the guild!')
+	await member.create_dm()
+	await member.dm_channel.send(
+		f'BARK BARK BARK BARK BARK'
+	)
+
+@client.event
+async def on_member_remove(member):
+	print(f'{member.name} has left the guild!')
 
 client.run(TOKEN)
